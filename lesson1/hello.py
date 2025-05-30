811cdfc412e985ed6b8fafb57bf926e0e708cb94
Abdikadir <abdikadir013@icloud.com>
2025-05-30 21:44:31 +0100

    test1

--- lesson1/hello.py
+++ lesson1/hello.py
@@ -1,4 +1,4 @@
 greeting = 'hello world'
 print(greeting)
 
-# In order to run this code, you can use the command:python3 hello.py
+# In order to run this code, you can use the command:python3 hello.py  
